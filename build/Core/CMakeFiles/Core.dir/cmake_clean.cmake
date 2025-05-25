272863604c3838d6cba66898eb07fca349e96953
THEDEGENER <luke.howe212@gmail.com>
2025-05-25 10:35:41 +1000

    rebuilt cmake build

---
+++ build/Core/CMakeFiles/Core.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Core.dir/Vendor/glad/glad.c.o"
+  "CMakeFiles/Core.dir/Vendor/glad/glad.c.o.d"
+  "CMakeFiles/Core.dir/src/Application/Application.cpp.o"
+  "CMakeFiles/Core.dir/src/Application/Application.cpp.o.d"
+  "CMakeFiles/Core.dir/src/Logging/Logging.cpp.o"
+  "CMakeFiles/Core.dir/src/Logging/Logging.cpp.o.d"
+  "CMakeFiles/Core.dir/src/window/Window.cpp.o"
+  "CMakeFiles/Core.dir/src/window/Window.cpp.o.d"
+  "libCore.a"
+  "libCore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
